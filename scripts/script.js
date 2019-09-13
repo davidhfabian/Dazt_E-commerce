2e0da9ed41f66c696e5743e6a83d81f040cf5056
David Fabian <davidfabianok@gmail.com>
2019-09-13 01:05:13 -0300

    Agrega slider : Home

--- scripts/script.js
+++ scripts/script.js
@@ -23,6 +23,28 @@ grid.addEventListener('mouseleave', () => {
 	}
 }); //desapaece si se sale del grid
 
+
+// ------------------SLIDER----------------------------
+arraySlider = Array ('../recursos/slider1.jpg','../recursos/slider2.jpg','../recursos/slider3.jpg');
+var i = 0;
+function slider(){
+    document.slider.src = arraySlider[i];
+    if(i < arraySlider.length -1){
+        i++;
+    } else {
+        i=0;
+    }
+
+    setTimeout("slider()", 3000);
+}
+
+
+window.onload = slider(); 
+
+
+
+
+
 // ------------------NAV GRID SUBCATEGORIAS----------------------------
 
 var categoria = document.querySelectorAll('.container-categorias a');
